f7f068374aeef1a86188bd834d4522fb1567363b
Daniel Crovo <danielcrovop@gmail.com>
2022-10-05 20:48:01 -0500

    C software added, not doing what it is expected to do

---
+++ workspace/avalon_div/software/nios_divc/obj/default/hello_world.d
@@ -0,0 +1,8 @@
+obj/default/hello_world.o: hello_world.c ../nios_divc_bsp/system.h \
+ ../nios_divc_bsp/linker.h ../nios_divc_bsp//HAL/inc/alt_types.h \
+ ../nios_divc_bsp//HAL/inc/io.h ../nios_divc_bsp//HAL/inc/alt_types.h
+../nios_divc_bsp/system.h:
+../nios_divc_bsp/linker.h:
+../nios_divc_bsp//HAL/inc/alt_types.h:
+../nios_divc_bsp//HAL/inc/io.h:
+../nios_divc_bsp//HAL/inc/alt_types.h:
